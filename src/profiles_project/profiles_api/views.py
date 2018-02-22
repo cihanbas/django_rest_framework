a476962b85df2e93b60ddbb454b238ab6ed51368
Cihan Baş <cihan.bas@doruk.net.tr>
2018-02-22 14:49:34 +0300

    Added View ,Urls and project urls it is working

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response 
 
-# Create your views here.
+
+class HelloApiView (APIView):
+    def get(self,request,format=None):
+      an_api_view=[
+        'Uses Http Method sdklsdfk lsdf sf ',
+        'sadsada sdsad ad asd sad ',
+        'sfskldfj kdsjfmskdjf ksdf sdjf ',
+        'sdfsd fdfk ldfkdlfk fkdlf kdf '
+      ]
+      return Response({'message':'hello Spi ','an_api_view':an_api_view})
